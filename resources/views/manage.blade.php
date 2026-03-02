298770c4cb21850433ba6dd1672be6fe0d0c776b
Dhruv <dhruv.p7115@gmail.com>
2026-03-02 18:56:33 -0500

    Dispalyed inventory data in a table on manage page

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -1,4 +1,33 @@
 @extends('layout')
 
 @section('content')
-  @endsection
+    <h2>Manage Inventory</h2>
+    
+    <div class="mb-3">
+        <a href="{{ url('/manage?action=create') }}" class="btn btn-primary">Create New Item</a>
+    </div>
+
+    <table class="table table-bordered table-striped">
+        <thead class="table-dark">
+            <tr>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Code</th>
+                <th>Price</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($items as $item)
+                <tr>
+                    <td>{{ $item->name }}</td>
+                    <td>{{ $item->description }}</td>
+                    <td>{{ $item->product_code }}</td>
+                    <td>${{ number_format($item->price, 2) }}</td>
+                    <td>
+                        </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
