b40bdc337a49b8aeb3c86ad525cda2c825298af3
Dhruv <dhruv.p7115@gmail.com>
2026-03-02 18:38:52 -0500

    In web.php page, added POST, GET, PUT and DELETE routes for the manage page

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,15 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
 
-
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/', function () { return view('home'); });
+Route::get('/about', function () { return view('about'); });
 
 
 Route::get('/manage', [ItemController::class, 'index']);
-
+Route::post('/manage', [ItemController::class, 'store']);
+Route::put('/manage/{item}', [ItemController::class, 'update']);
+Route::delete('/manage/{item}', [ItemController::class, 'destroy']);
 
 
 Route::get('/search', [ItemController::class, 'searchPage']);
