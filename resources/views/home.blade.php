8512f9bb2fa10aed5aae7160e06464c7496f7912
Dhruv <dhruv.p7115@gmail.com>
2026-03-02 18:12:49 -0500

    Implemented Home page UI with inviting text and inventory image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,10 @@
 @extends('layout')
 
 @section('content')
-  @endsection
+    <div class="text-center mt-5">
+        <h2>Welcome to InventoryPro</h2>
+        <p class="lead mt-3">Try out our powerful application to seamlessly manage, track, and search your entire inventory collection.</p>
+        
+        <img src="https://images.unsplash.com/photo-1586528116311-ad8ed7c1590a?w=800&q=80" alt="Warehouse Inventory" class="img-fluid rounded shadow mt-4" style="max-height: 400px;">
+    </div>
+@endsection
